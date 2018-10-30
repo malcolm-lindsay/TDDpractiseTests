40812911f102774efb8dcf8a9383d93ccd652862
Malcolm-Lindsay <malcolm-lindsay@hotmail.com>
2018-10-30 10:37:12 +0000

    dcsdcsd

---
+++ src/main/java/accapp/Runner.java
@@ -0,0 +1,10 @@
+package accapp;
+
+
+
+public class Runner {
+	public static void main(String[] args) {
+
+		System.out.println("Hello world to the standard out");
+	}
+}
--- src/main/java/accapp/app.java
+++
@@ -1,5 +0,0 @@
-package accapp;
-
-public class app {
-
-}
